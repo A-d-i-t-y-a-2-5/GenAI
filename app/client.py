813e2e0ae572bbeb00b00fa2b30a1cae097c52e5
A-d-i-t-y-a-2-5 <adityapratapsaha@gmail.com>
2026-03-24 07:46:37 +0000

    Added streamlit GUI

---
+++ app/client.py
@@ -0,0 +1,24 @@
+import requests
+import streamlit as st
+
+st.title("ChatBot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
+if prompt := st.chat_input("Write your prompt in this input field"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    with st.chat_message("user"):
+        st.text(prompt)
+        
+    response = requests.get("http://localhost:8000/chat/sync", params={"query": prompt})
+    
+    response.raise_for_status()
+    
+    with st.chat_message("assistant"):
+        st.markdown(response.json()["response"])
