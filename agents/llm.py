dfba52e5bf4a5376dfc05c3ba752f4a2182b3ef2
A-d-i-t-y-a-2-5 <adityapratapsaha@gmail.com>
2026-04-08 06:56:37 +0000

    Added client interface and LLM functions

---
+++ agents/llm.py
@@ -0,0 +1,62 @@
+from langchain_core.documents import Document
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_openrouter import ChatOpenRouter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+from agents.schema import JobPostingList
+
+
+class LanguageModel:
+    def __init__(self, config: BaseSettings):
+        self.config = config
+
+        model = ChatOpenRouter(
+            model_name=f"{config.llm.provider}/{config.llm.model}",
+            api_key=config.llm.api_key,
+            temperature=config.llm.temperature,
+            max_tokens=config.llm.max_tokens,
+            timeout=config.llm.timeout,
+            max_retries=config.llm.max_retries,
+        )
+
+        self.structured_model = model.with_structured_output(JobPostingList)
+
+        self.splitter = RecursiveCharacterTextSplitter(
+            separators=[config.extractor.separator],  # exact phrase to split on
+            chunk_size=config.extractor.chunk_size,
+            chunk_overlap=0,
+            is_separator_regex=False,
+        )
+
+        self.system_message = SystemMessage(
+            content="Extract structured data from the job posting  present in the webpage content below. Use null or empty string for missing fields depending on the schema."
+        )
+        
+    def split_document(self, doc: Document) -> list[Document]:
+        chunks = self.splitter.split_documents([doc])
+        print(f"Total chunks created: {len(chunks)}")
+        return chunks
+    
+    def generate_inputs(self, chunks: list[Document]) -> list[list[SystemMessage | HumanMessage]]:
+        return [
+            [
+                self.system_message,
+                HumanMessage(content=chunk.page_content),
+            ]
+            for chunk in chunks
+        ]
+        
+    def merge_results(self, results: list[JobPostingList]) -> JobPostingList:
+        all_jobs = []
+        for result in results:
+            all_jobs.extend(result.jobs)
+        
+        return JobPostingList(jobs=all_jobs)
+    
+    def generate_response(self, inputs: list[list[SystemMessage | HumanMessage]]) -> JobPostingList:
+        results = self.structured_model.batch(inputs, config={"max_concurrency": 5})
+        return self.merge_results(results)
+    
+    
---
+++ app/client.py
@@ -0,0 +1,56 @@
+import os
+
+import pandas as pd
+import streamlit as st
+
+from agents.extractor import get_linkedin_page
+from agents.llm import LanguageModel
+from app.settings import AppSettings
+from app.utils import copy_firefox_cookies, load_cookies_from_file
+
+config = AppSettings()
+llm = LanguageModel(config)
+
+copy_firefox_cookies(config.os_username, os.curdir)
+cookies = load_cookies_from_file(
+    os.path.join(os.curdir, "copied_cookies.sqlite"), domain_name=".linkedin.com"
+)
+
+
+st.set_page_config(layout="wide")
+num_rows = 25
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    title = st.text_input(
+        "Keywords", placeholder="Specify keywords to search for in the LinkedIn profile"
+    )
+with col2:
+    sort_by = st.selectbox(
+        "Sort by",
+        ["relevance", "date_posted"],
+        format_func=lambda x: x.replace("_", " ").title(),
+    )
+with col3:
+    date_posted = st.selectbox(
+        "Date Posted",
+        ["past-24h", "past-week", "past-month"],
+        format_func=lambda x: x.replace("-", " ").title(),
+    )
+
+if st.button("Submit", type="primary", use_container_width=True):
+    with st.spinner("Extracting job postings from LinkedIn...", show_time=True):
+        doc = get_linkedin_page(
+            f'https://www.linkedin.com/search/results/content/?keywords={title}&sortBy=["{sort_by}"]&datePosted=["{date_posted}"]',
+            cookies=cookies,
+            password=config.extractor.password,
+            scroll_duration=config.extractor.scroll_duration,
+        )
+
+    inputs = llm.split_document(doc)
+    response = llm.generate_response(llm.generate_inputs(inputs))
+    records = [job.model_dump() for job in response.jobs]
+    df = pd.DataFrame(records)
+
+    st.dataframe(df, height=(num_rows + 1) * 35 + 3)
